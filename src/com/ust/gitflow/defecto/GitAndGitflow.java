389ea5f33764fcb03aebfb77cb45c49f1bf0d008
xjossanc <jose.sanchez.tamborero@itbo-ericsson.com>
2021-04-12 10:24:54 +0200

    Commit inicial del proyecto

---
+++ src/com/ust/gitflow/defecto/GitAndGitflow.java
@@ -0,0 +1,12 @@
+package com.ust.gitflow.defecto;
+
+public class GitAndGitflow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Unificacion de Git y GitFlow");
+		
+	}
+
+}
---
+++ src/com/ust/gitflow/defecto/PruebaGitflow.java
@@ -0,0 +1,12 @@
+package com.ust.gitflow.defecto;
+
+public class PruebaGitflow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Esto es un proyecto de prueba de GitFlow");
+		
+	}
+
+}
